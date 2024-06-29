5cd5dec1bb6c34d10c1b63440310ba891ab03c63
Mehtor99 <mehtor99@gmail.com>
2024-06-29 13:32:53 +0300

    aaa

---
+++ src/main/java/org/mehtor/SlaytOrnek.java
@@ -0,0 +1,16 @@
+package org.mehtor;
+
+public class SlaytOrnek {
+    public static void main(String[] args) {
+        int x =20;
+        double y = 40.5;
+        long p = 30;
+        float q = 10.60f;
+        //int z = x+y;  //Error; cannot convert from double to int  //hata; doubleden int e donusum yapilamiyor.
+        double z = x + y;
+        System.out.println("Toplam: " + z);
+        //long r = p - q;  //Error; cannot convert from float to long  //hata: floattan long a donusum yapilamiyor.
+        float r = p- q;
+        System.out.println("Fark: "+ r);
+    }
+}
